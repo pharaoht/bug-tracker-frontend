a51220c12c805c8b848027733762e94cfb5c6d3c
pharaoh <pharaohmanson@gmail.com>
2024-05-03 16:21:43 -0400

    table component html css

--- components/Main/Main.tsx
+++ components/Main/Main.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import styles from './main.module.css';
 import Breadcrumb from '../Breadcrumb/Breadcrumb';
 import Widget from '../Widget/Widget';
+import Table from '../Table/Table';
 
 const Main = () => {
   return (
     <div className={styles.main}>
         <Breadcrumb/>
         <Widget/>
+        <Table/>
     </div>
   )
 }
--- components/Table/Table.tsx
+++ components/Table/Table.tsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React from 'react';
+import styles from './table.module.css';
 
 const Table = () => {
   return (
-    <div>Table</div>
+        <div className={styles.tableData}>
+            <div className={styles.order}>
+                <div className={styles.head}>
+                    <h3>Recent Orders</h3>
+                    <i className={styles.bx} >v</i>
+                    <i className={styles.bx}>x</i>
+                </div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>User</th>
+                            <th>Date Order</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                photo
+                                <p>John Doe</p>
+                            </td>
+                            <td>01-10-2021</td>
+                            <td><span className={styles.statusCompleted}>Completed</span></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
   )
 }
 
