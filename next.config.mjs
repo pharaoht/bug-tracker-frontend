a926ebf4d79a3eec3b4ca40095be6b1ce1ee5c22
pharaoh <pharaohmanson@gmail.com>
2024-05-11 15:15:34 -0400

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,8 @@
-const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
         domains: ['res.cloudinary.com']
-    },
-
-    webpack: (config) => {
-        config.resolve.alias['@'] = path.join(__dirname, 'components');
-        return config;
-    },
+    }
 };
 
 export default nextConfig;
